8c58a1f3a77241dcc56162e8b3f6d0c659858eb6
AlesonTalis <alesontor@gmail.com>
2022-06-12 02:40:42 -0300

    banner

--- src/ScreenParts/index.js
+++ src/ScreenParts/index.js
@@ -2,3 +2,7 @@
 export {
     default as Header
 } from './HeaderMenu'
+
+export {
+    default as Banner
+} from './PageBanner'
--- src/Screens/HomeScreen/index.jsx
+++ src/Screens/HomeScreen/index.jsx
@@ -1,10 +1,18 @@
-
 import React from 'react'
 
+import {
+    Banner
+} from '../../ScreenParts'
+
 function HomeScreen() {
-  return (
-    <div className="Content">HomeScreen</div>
-  )
+    return (
+        <>
+            <Banner />
+            <div className="Content">
+                <h1>HomeScreen</h1>
+            </div>
+        </>
+    )
 }
 
 export default HomeScreen

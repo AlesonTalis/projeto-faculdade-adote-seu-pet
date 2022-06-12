c77bd72c402641625f6d304eb7f81b5237d8d073
AlesonTalis <alesontor@gmail.com>
2022-06-12 02:07:01 -0300

    header

--- src/ScreenParts/HeaderMenu/index.jsx
+++ src/ScreenParts/HeaderMenu/index.jsx
@@ -1,10 +1,49 @@
 
 import React from 'react'
 
+const linkies = [
+    {
+        name: 'Adote',
+        link: '#'
+    },
+    {
+        name: 'Doar',
+        link: '#'
+    },
+    {
+        name: 'Sobre',
+        link: '#'
+    },
+    {
+        name: 'Contato',
+        link: '#'
+    }
+]
+
 function HeaderMenu() {
   return (
     <div className="Header">
-        <div className="Content">HeaderMenu</div>
+        <div className="Content">
+            <a href="#" className="Link">
+                <h1>Adote um Pet</h1>
+            </a>
+            <ul>
+                {linkies.map((link, index) => (
+                    <li key={index}>
+                        <a href={link.link}>{link.name}</a>
+                    </li>
+                ))}
+            </ul>
+            <div className="LoginArea">
+                <a href="#" className="LinkMenu">
+                    <h1>Login</h1>
+                </a>
+                <span>|</span>
+                <a href="#" className="LinkMenu">
+                    <h1>Cadastrar</h1>
+                </a>
+            </div>
+        </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@ module.exports = {
     content: [
         "./index.html",
         "./src/**/*.{vue,js,ts,jsx,tsx}",
-        ],
+    ],
     theme: {
         extend: {
-            
+            colors: {
+                main: '#ffffff',
+                primary: '#00a8ff',
+                secondary: '#4cd137',
+                text: '#2f3640'
+            }
         },
     },
     plugins: [],
